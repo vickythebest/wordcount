5883e04309ae6b8f6378e7b69aa70d7478355185
vivek kumar <vickythebest008@gmail.com>
2023-08-24 13:53:29 -0700

    Dummy kafka stream project

---
+++ src/main/java/com/github/coolhand/kafka/streams/App.java
@@ -0,0 +1,13 @@
+package com.github.coolhand.kafka.streams;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
