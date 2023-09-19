198fe9c592469fccec21d245c4b4440b4e495ef1
vivek kumar <vickythebest008@gmail.com>
2023-09-19 12:34:38 -0700

    upgraded wordcount with kafka stream 3.5.1

--- src/main/java/com/github/coolhand/kafka/streams/StreamsStarterApp.java
+++ src/main/java/com/github/coolhand/kafka/streams/StreamsStarterApp.java
@@ -3,10 +3,10 @@ package com.github.coolhand.kafka.streams;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KStreamBuilder;
-import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.*;
 
 import java.util.Arrays;
 import java.util.Properties;
@@ -15,8 +15,42 @@ import java.util.Properties;
  * Hello Kafka Stream!
  *
  */
+
 public class StreamsStarterApp
 {
+    public Topology createTopology(){
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+//        1. Stream
+        KStream<String, String> wordCountInput = streamsBuilder.stream("word-count-input");
+
+        wordCountInput.print(Printed.toSysOut());
+
+        var wordCounts = wordCountInput
+//        2. Map the key & value in lowercase
+                .mapValues(textLine -> textLine.toLowerCase())
+//        3. FlatMapValue to split the message by space
+                .flatMapValues(textLine -> Arrays.asList(textLine.split("\\W+")))
+//        4. SelectKey change the Key name similar to value
+                .selectKey((key, word) -> word)
+//        5. GroupKey group the key to count the words
+                .groupByKey()
+//        6. Count
+                .count(Materialized.as("Counts"));
+//        7. TO Save the msg to back to kafka under target topic
+
+
+        wordCounts.toStream().print(Printed.toSysOut());
+
+//        wordCounts.toStream().foreach((key,value)->{
+//            System.out.println("Key: "+key+ ", Value: "+value);
+//        });
+
+        wordCounts.toStream().to("word-count-outpu", Produced.with(Serdes.String(),Serdes.Long()));
+
+        return streamsBuilder.build();
+
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello Kafka Stream!" );
@@ -28,31 +62,9 @@ public class StreamsStarterApp
         properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,Serdes.String().getClass());
 
-        KStreamBuilder builder = new KStreamBuilder();
-//        1. Stream
-        KStream<String, String> wordCountInput = builder.stream("word-count-input");
+        StreamsStarterApp streamsStarterApp= new StreamsStarterApp();
 
-        KTable<String, Long> wordCounts = wordCountInput
-//        2. Map the key & value in lowercase
-                .mapValues(textLine -> textLine.toLowerCase())
-//        3. FlatMapValue to split the message by space
-                .flatMapValues(textLine -> Arrays.asList(textLine.split("\\W+")))
-//        4. SelectKey change the Key name similar to value
-                .selectKey((key, word) -> word)
-//        5. GroupKey group the key to count the words
-                .groupByKey()
-//        6. Count
-                .count("Counts");
-
-//        7. TO Save the msg to back to kafka under target topic
-
-        wordCounts.toStream().foreach((key,value)->{
-            System.out.println("Key: "+key+ ", Value: "+value);
-        });
-
-        wordCounts.to(Serdes.String(),Serdes.Long(),"word-count-output");
-
-        KafkaStreams streams=new KafkaStreams(builder,properties);
+        KafkaStreams streams=new KafkaStreams(streamsStarterApp.createTopology(),properties);
         streams.start();
         //  Printed topologies
         System.out.println("Printed topologies: "+streams.toString());
